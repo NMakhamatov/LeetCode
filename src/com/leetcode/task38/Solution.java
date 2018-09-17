873cb760ef8304b165ba5076b55bb7119a350c0f
“Nursultan <nmakhamatov@tsconsulting.ru>
2018-09-17 16:47:56 +0300

    done

--- src/com/leetcode/task38/Solution.java
+++ src/com/leetcode/task38/Solution.java
@@ -1,20 +1,36 @@
 package com.leetcode.task38;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
+//38. Count and Say
+//done
 public class Solution {
     public static void main(String[] args) {
-        String s = "Hello";
-        List<Character> list = s.chars().boxed().map(x -> (char)x.intValue()).collect(Collectors.toList());
-        System.out.println(list.toString());
+        Solution solution = new Solution();
+        System.out.println(solution.countAndSay(2));
+        System.out.println(solution.countAndSay(3));
+        System.out.println(solution.countAndSay(4));
+        System.out.println(solution.countAndSay(6));
     }
     public String countAndSay(int n) {
         if (n == 1) return "1";
-        countAndSay(n-1);
+
+        String prevRes = countAndSay(n-1);
+        char[] chars = prevRes.toCharArray();
+        int value = 0;
+        int amount = 0;
         StringBuilder builder = new StringBuilder();
-        int i = n/10;
+
+        for (char c : chars) {
+            int currentValue = (int)(c - '0');
+            if (value != currentValue) {
+                if (value != 0) {
+                    builder.append(amount).append(value);
+                }
+                value = currentValue;
+                amount = 1;
+            }
+            else amount++;
+        }
+        builder.append(amount).append(value);
+        return builder.toString();
     }
 }
